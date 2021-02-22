3c2519f31bd16470a0649630cea923b24656f89b
Ivan Chien <qyc027@gmail.com>
2021-02-23 00:29:55 +0800

    Update early-init: add proxy settings

--- early-init.el
+++ early-init.el
@@ -1,5 +1,10 @@
 ;;; early-init.el --- -*- lexical-binding: t -*-
 
+;; 代理设置
+(setq url-proxy-services
+      '(("http" . "127.0.0.1:7890")
+        ("https" . "127.0.0.1:7890")))
+
 (setq user-init-file (or load-file-name (buffer-file-name)))
 (setq user-emacs-directory (file-name-directory user-init-file))
 
