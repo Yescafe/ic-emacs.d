2a5fe29084c7d9f3f38c61a3788e12a344f72148
Ivan Chien <qyc027@gmail.com>
2021-02-23 02:38:47 +0800

    Update early-init for lower version Emacs

--- early-init.el
+++ early-init.el
@@ -1,15 +1,5 @@
 ;;; early-init.el --- -*- lexical-binding: t -*-
 
-;; 代理设置
-(setq url-proxy-services
-      '(("http" . "127.0.0.1:7890")
-        ("https" . "127.0.0.1:7890")))
-
-(setq user-init-file (or load-file-name (buffer-file-name)))
-(setq user-emacs-directory (file-name-directory user-init-file))
-
-(add-to-list 'load-path (expand-file-name "parts/" user-emacs-directory))
-
 ;; startup speed up
 (setq gc-cons-threshold 100000000)
 
--- init.el
+++ init.el
@@ -6,6 +6,15 @@
 ;;; Codes:
 
 ;; Environments
+(setq url-proxy-services
+      '(("http" . "127.0.0.1:7890")
+        ("https" . "127.0.0.1:7890")))
+
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+
+(add-to-list 'load-path (expand-file-name "parts/" user-emacs-directory))
+
 (defconst *macos* (eq system-type 'darwin))
 (defconst *linux* (eq system-type 'gnu/linux))
 (defconst *windows* (or (eq system-type 'windows-nt)
