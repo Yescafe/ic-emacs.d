f5aa20004338494b74b39b23c9c3d80e7c613df2
Ivan Chien <qyc027@gmail.com>
2021-02-23 01:45:36 +0800

    Add startup optimizations

--- early-init.el
+++ early-init.el
@@ -10,4 +10,20 @@
 
 (add-to-list 'load-path (expand-file-name "parts/" user-emacs-directory))
 
+;; startup speed up
+(setq gc-cons-threshold 100000000)
+
+(setq package-enable-at-startup nil)
+
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(setq site-run-file nil)
+
+(unless (and (display-graphic-p) (eq system-type 'darwin))
+  (push '(menu-bar-lines . 0) default-frame-alist))
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+
 (provide 'early-init)
